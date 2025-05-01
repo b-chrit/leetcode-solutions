ce7047e96d921c80ce8d1bb6eb3aff82e72c0a1c
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-01 15:45:14 -0400

    Add solution for 139 - tabularization sol

--- Dynamic Programming/139_Word_Break.java
+++ Dynamic Programming/139_Word_Break.java
@@ -15,4 +15,24 @@ class Solution {
         memo.put(s, false);
         return memo.get(s);
     }
+
+
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] table = new boolean[s.length() + 1];
+        table[0] = true;
+        
+        for (int i = 0; i <= s.length(); i++) {
+            if (table[i]) {
+                for (String word : wordDict) {
+                    if (i + word.length() <= s.length() && 
+                        s.substring(i, i + word.length()).equals(word)) {
+                        table[i + word.length()] = true;
+                    }
+                }
+            }
+        }
+        
+        return table[s.length()];
+    }
 }
