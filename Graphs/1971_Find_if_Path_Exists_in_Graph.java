f965adec12de5cd9135139961b318a95f784dd7d
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-05 17:34:43 -0400

    Add solution for 1971

--- Graphs/1971_Find_if_Path_Exists_in_Graph.java
+++ Graphs/1971_Find_if_Path_Exists_in_Graph.java
@@ -1,5 +1,35 @@
-package Graphs;
+class Solution {
+    public List<Integer>[] buildGraph(int[][] edges, int n) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        return graph;
+    }
 
-public class 1971_Find_if_Path_Exists_in_Graph {
-    
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] graph = buildGraph(edges, n);
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (current == destination) {
+                return true;
+            }
+            for (int neighbor : graph[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return false;
+    }
 }
