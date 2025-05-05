6c387e999d94d6104a4b7e8200dda40bec1bafa2
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-05 18:03:47 -0400

    Add solution for 2685

--- Graphs/2685_Count_the_Number_of_Complete_Components.java
+++ Graphs/2685_Count_the_Number_of_Complete_Components.java
@@ -44,4 +44,19 @@ class Solution {
         }
         return true;
     }
+
+    private List<Integer>[] buildGraph(int n, int[][] edges) {
+        List<Integer>[] graph = new ArrayList[n];
+        
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        
+        return graph;
+    }
 }
