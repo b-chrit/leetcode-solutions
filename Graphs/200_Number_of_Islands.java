6fcc25eba014745dd0f8ca7cd654cc3db1c202be
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-05 20:25:24 -0400

    Add solution for 2685

--- Graphs/200_Number_of_Islands.java
+++ Graphs/200_Number_of_Islands.java
@@ -15,4 +15,17 @@ class Solution {
 
         return count;
     }
+
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != '1') {
+            return;  
+        }
+
+        grid[row][col] = '0'; 
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
 }
