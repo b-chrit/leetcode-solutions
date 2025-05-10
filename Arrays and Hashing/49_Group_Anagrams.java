501e204fba28031bb59f0d7f59b68c66e1150a45
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-10 12:18:49 -0400

    Add solution for 49

---
+++ Arrays and Hashing/49_Group_Anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
