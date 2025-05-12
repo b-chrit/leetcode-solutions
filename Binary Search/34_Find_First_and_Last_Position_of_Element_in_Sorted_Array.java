c9f1ea773b615e319b9bd814d7677059f0fb4c3c
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-12 18:34:02 -0400

    Add solution for 34

--- Binary Search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Binary Search/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,3 +1,51 @@
-public class 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[]{-1, -1};
+        
+        int firstPos = findFirstPosition(nums, target);
+        if (firstPos == -1) return new int[]{-1, -1};
+        
+        int lastPos = findLastPosition(nums, target);
+        return new int[]{firstPos, lastPos};
+    }
     
+    private int findFirstPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                result = mid; 
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    private int findLastPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                result = mid;  
+                left = mid + 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
 }
