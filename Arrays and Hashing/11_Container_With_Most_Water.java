19f26e0a0e35d53df8c1c4cffacf9cde47ef9065
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-13 16:16:40 -0400

    Add solution for 11

---
+++ Arrays and Hashing/11_Container_With_Most_Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int result = Integer.MIN_VALUE;
+
+        while(left <= right){
+            int area = Math.min(height[left], height[right]) * (right - left);
+            result = Math.max(result, area);
+            
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right --;
+            }
+        }
+
+        return result;
+    }
+}
