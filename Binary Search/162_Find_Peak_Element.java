a05fb33331b4318636d34c963350c6384c0070c5
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-12 20:00:55 -0400

    Add solution for 162

--- Binary Search/162_Find_Peak_Element.java
+++ Binary Search/162_Find_Peak_Element.java
@@ -1,3 +1,13 @@
-public class 162_Find_Peak_Element {
-    
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+
+        return left;
+    }
 }
