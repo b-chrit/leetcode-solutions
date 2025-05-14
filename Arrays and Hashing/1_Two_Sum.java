bcd8a94bc73a75372a8c11f716b1ca874a4cb7fe
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-05-14 18:41:02 -0400

    Add solution for 1 - optimized

--- Arrays and Hashing/1_Two_Sum.java
+++ Arrays and Hashing/1_Two_Sum.java
@@ -1,11 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target) return new int[]{i, j};
+        Map<Integer, Integer> myMap = new HashMap<>();
+        int difference = 0;
+        for(int i =0; i < nums.length; i++){
+            difference = target - nums[i];
+            if(myMap.containsKey(difference)){
+                return new int[]{myMap.get(difference), i};
             }
+            myMap.put(nums[i], i);
         }
         return new int[]{};
     }
-    
 }
