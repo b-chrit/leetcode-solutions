e6973f5921f082f8eea00b872e38ef1ee5dce7eb
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-18 17:50:16 -0400

    Add solution for 70

--- Dynamic Programming/70_Climbing_Stairs.java
+++ Dynamic Programming/70_Climbing_Stairs.java
@@ -1,3 +1,13 @@
-public class 70_Climbing_Stairs {
+class Solution {
+    HashMap<Integer, Integer> myMap = new HashMap<>();
     
+    public int climbStairs(int n) {
+        if(myMap.containsKey(n)) return myMap.get(n);
+        if(n == 0) return 1;
+        if(n < 0 ) return 0;
+
+        int result = climbStairs(n-1) + climbStairs(n-2);
+        myMap.put(n, result);
+        return result;
+    }
 }
