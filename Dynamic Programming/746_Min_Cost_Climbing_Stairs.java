eae11af85831abe1435a3729f2917d8d1c74680a
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-18 17:36:46 -0400

    Add solution for 746

--- Dynamic Programming/746_Min_Cost_Climbing_Stairs.java
+++ Dynamic Programming/746_Min_Cost_Climbing_Stairs.java
@@ -1,3 +1,13 @@
-public class 746_Min_Cost_Climbing_Stairs {
-    
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        cost = java.util.Arrays.copyOf(cost, n + 1);
+        cost[n] = 0;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        }
+        
+        return Math.min(cost[0], cost[1]);
+    }
 }
