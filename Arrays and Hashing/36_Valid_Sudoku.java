6cc839fb30b42c4b956dda98d0678dee2b1f4cc6
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-18 17:34:15 -0400

    Add solution for 36

--- Arrays and Hashing/36_Valid_Sudoku.java
+++ Arrays and Hashing/36_Valid_Sudoku.java
@@ -1,3 +1,18 @@
-public class 36_Valid_Sudoku {
-    
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Check rows
+        for (int i = 0; i < 9; i++) {
+            Set<Character> rowSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                char currentChar = board[i][j];
+                if (currentChar != '.') {
+                    if (rowSet.contains(currentChar)) {
+                        return false;
+                    }
+                    rowSet.add(currentChar);
+                }
+            }
+        }
+        
+    }
 }
