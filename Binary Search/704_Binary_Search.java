e2d5278f8ebcdfa906db7b43b4140795f2c26a20
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-18 17:40:41 -0400

    Add solution for iterative bs

--- Binary Search/704_Binary_Search.java
+++ Binary Search/704_Binary_Search.java
@@ -1,18 +1,22 @@
 class Solution {
-    public int search(int[] nums, int target) {
-    //   int left = 0;
-    //   int right = nums.length - 1;
 
-    //   while(left <= right){
-    //     int middleIndex = left + (right - left) / 2;
-    //     if(nums[middleIndex] == target) return middleIndex;
-    //     if(target < nums[middleIndex]){
-    //         right = middleIndex - 1;
-    //     }else{
-    //         left = middleIndex + 1;
-    //     }
-    //   }
-    //   return -1;
+    public int search(int[] nums, int target) {
+      int left = 0;
+      int right = nums.length - 1;
+
+      while(left <= right){
+        int middleIndex = left + (right - left) / 2;
+        if(nums[middleIndex] == target) return middleIndex;
+        if(target < nums[middleIndex]){
+            right = middleIndex - 1;
+        }else{
+            left = middleIndex + 1;
+        }
+      }
+      return -1;
+    }
+
+    public int search(int[] nums, int target) {
     if(nums == null || nums.length == 0) return -1;
     return search(nums, target, 0, nums.length - 1);
     }
