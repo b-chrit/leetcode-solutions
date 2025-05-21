a962ea8d93ddd528e282418cb05d7238d4843742
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-21 17:29:45 -0400

    Add solution for 875

--- Binary Search/875_Koko_Eating_Bananas.java
+++ Binary Search/875_Koko_Eating_Bananas.java
@@ -1,3 +1,38 @@
-public class 875_Koko_Eating_Bananas {
+class Solution {
+    public int minEatingSpeed(int[] piles, int maxHours) {
+        int maxBananas = 0;
+        for (int pile : piles) {
+            maxBananas = Math.max(maxBananas, pile);
+        }
+        
+        return findMinimumSpeed(piles, maxHours, 1, maxBananas);
+    }
     
+    private int findMinimumSpeed(int[] piles, int maxHours, int minSpeed, int maxSpeed) {
+        if (minSpeed >= maxSpeed) {
+            return minSpeed;
+        }
+        
+        int midSpeed = minSpeed + (maxSpeed - minSpeed) / 2;
+        
+        if (canEatAllBananas(piles, maxHours, midSpeed)) {
+            return findMinimumSpeed(piles, maxHours, minSpeed, midSpeed);
+        } else {
+            return findMinimumSpeed(piles, maxHours, midSpeed + 1, maxSpeed);
+        }
+    }
+    
+    private boolean canEatAllBananas(int[] piles, int maxHours, int speed) {
+        int hoursRequired = 0;
+        
+        for (int pile : piles) {
+            hoursRequired += Math.ceil((double) pile / speed); // (pile + speed - 1) / speed
+            
+            if (hoursRequired > maxHours) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
