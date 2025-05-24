700d9695a0f6263ad4bac99e0522f10dc5e99be1
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-23 20:03:11 -0400

    Add solution for 994

--- Graphs/994_Rotting_Oranges.java
+++ Graphs/994_Rotting_Oranges.java
@@ -1,3 +1,23 @@
-public class 994_Rotting_Oranges {
-    
+import java.util.*;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> rottenQueue = new LinkedList<>();
+        int freshOranges = 0;
+        
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == 2) {
+                    rottenQueue.offer(new int[]{row, col});
+                } else if (grid[row][col] == 1) {
+                    freshOranges++;
+                }
+            }
+        }
+        
+        if (freshOranges == 0) return 0;
+        
+    }
 }
