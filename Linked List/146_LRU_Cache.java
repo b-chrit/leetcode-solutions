e112338ae7a5b1187457855ddd34d0ea64ff41a8
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-25 09:33:27 -0400

    Add solution for 146

--- Linked List/146_LRU_Cache.java
+++ Linked List/146_LRU_Cache.java
@@ -1,3 +1,60 @@
-public class 146_LRU_Cache {
+public class LRUCache {
     
+    class Node {
+        int key, value;
+        Node prev, next;
+        
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    private int capacity;
+    private Map<Integer, Node> cache;
+    private Node head, tail;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            moveToHead(node);
+            return node.value;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            
+            if (cache.size() >= capacity) {
+                Node lastNode = removeTail();
+                cache.remove(lastNode.key);
+            }
+            
+            cache.put(key, newNode);
+            addToHead(newNode);
+        }
+    }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
