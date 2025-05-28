2381f2fcdb2a0165183da74a631215a8230b301b
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-27 21:21:34 -0400

    Add solution for 567

---
+++ Arrays and Hashing/567_Permutation_in_String.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        
+        int[] s1Frequency = new int[26];
+        for (char character : s1.toCharArray()) {
+            s1Frequency[character - 'a']++;
+        }
+        
+        int[] windowFrequency = new int[26];
+        int windowSize = s1.length();
+        
+        for (int index = 0; index < windowSize; index++) {
+            windowFrequency[s2.charAt(index) - 'a']++;
+        }
+        
+        if (hasIdenticalFrequencies(s1Frequency, windowFrequency)) {
+            return true;
+        }
+        
+        for (int rightIndex = windowSize; rightIndex < s2.length(); rightIndex++) {
+            windowFrequency[s2.charAt(rightIndex) - 'a']++;
+            int leftIndex = rightIndex - windowSize;
+            windowFrequency[s2.charAt(leftIndex) - 'a']--;
+            
+            if (hasIdenticalFrequencies(s1Frequency, windowFrequency)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
