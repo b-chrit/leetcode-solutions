d9eb2f4c5f293d4c69de9cb8d857b5c569d3e746
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-28 17:07:14 -0400

    Add solution for 442

--- Arrays and Hashing/442_Find_All_Duplicates_in_an_Array.java
+++ Arrays and Hashing/442_Find_All_Duplicates_in_an_Array.java
@@ -1,3 +1,22 @@
-public class 442_Find_All_Duplicates_in_an_Array {
-    
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) {
+            return duplicates;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int currentNumber = Math.abs(nums[i]);
+            int targetIndex = currentNumber - 1;
+            
+            if (nums[targetIndex] < 0) {
+                duplicates.add(currentNumber);
+            } else {
+                nums[targetIndex] = -nums[targetIndex];
+            }
+        }
+        
+        return duplicates;
+    }
 }
