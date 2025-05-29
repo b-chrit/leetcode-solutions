5adb8f889cf14017599749918de7ebfa53e38041
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-29 16:17:15 -0400

    Add solution for 153

--- Binary Search/153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ Binary Search/153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -1,3 +1,18 @@
-public class 153_Find_Minimum_in_Rotated_Sorted_Array {
-    
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start < end) {
+            int middle = start + (end - start) / 2;
+            
+            if (nums[middle] > nums[end]) {
+                start = middle + 1;
+            } else {
+                end = middle;
+            }
+        }
+        
+        return nums[start];
+    }
 }
