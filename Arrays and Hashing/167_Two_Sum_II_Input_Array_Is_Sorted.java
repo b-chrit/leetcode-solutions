2a1a6bcb40c611eeba0c67f776eecde8b139a465
Baraa Chrit <baraaadnan01@gmail.com>
2025-05-30 20:10:31 -0400

    Add solution for 167

--- Arrays and Hashing/167_Two_Sum_II_Input_Array_Is_Sorted.java
+++ Arrays and Hashing/167_Two_Sum_II_Input_Array_Is_Sorted.java
@@ -1,3 +1,17 @@
-public class 167_Two_Sum_II_Input_Array_Is_Sorted {
-    
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if(sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{};
+    }
 }
