e90adbcd7f2caf03f283ee4506add899f132eab2
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-03 16:48:35 -0400

    Add solution for 46

--- Dynamic Programming/46_Permutations.java
+++ Dynamic Programming/46_Permutations.java
@@ -1,3 +1,11 @@
-public class 46_Permutations {
-    
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+        
+        boolean[] used = new boolean[nums.length];
+        permute(nums, result, new ArrayList<>(), used);
+        
+        return result;
+    }
 }
