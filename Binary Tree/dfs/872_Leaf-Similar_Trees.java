7221c8b52e799415669956cd9e78911971bd0e61
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-06 13:07:59 -0400

    Add solution for 872

--- Binary Tree/dfs/872_Leaf-Similar_Trees.java
+++ Binary Tree/dfs/872_Leaf-Similar_Trees.java
@@ -1,3 +1,40 @@
-public class 872_Leaf-Similar_Trees {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> rootOneLeaves = new ArrayList<>();
+        List<Integer> rootTwoLeaves = new ArrayList<>();
+
+        getRootLeaves(root1, rootOneLeaves);
+        getRootLeaves(root2, rootTwoLeaves);
+
+        if(rootOneLeaves.size() != rootTwoLeaves.size()) return false;
+
+        for(int i = 0; i < rootOneLeaves.size(); i++){
+            if(!rootOneLeaves.get(i).equals(rootTwoLeaves.get(i))) return false;
+        }
+
+        return true;
+    }
+
+    private void getRootLeaves(TreeNode root, List<Integer> list){
+        if(root == null) return;
+        if(root.left == null && root.right == null) list.add(root.val);
+
+        getRootLeaves(root.left, list);
+        getRootLeaves(root.right, list);
+    }
 }
