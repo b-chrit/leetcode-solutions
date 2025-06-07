2851fd554c22662e980140423f0b9d717812a9a0
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-07 09:56:53 -0400

    Add solution for 144

--- Binary Tree/dfs/144_Binary_Tree_Preorder_Traversal.java
+++ Binary Tree/dfs/144_Binary_Tree_Preorder_Traversal.java
@@ -1,3 +1,33 @@
-public class 144_Binary_Tree_Preorder_Traversal {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preOrderTraversal(root, result);
+
+        return result;
+    }
+
+    private void preOrderTraversal(TreeNode root, List<Integer> result){
+        if(root == null) return;
+        
+        result.add(root.val);
+
+        preOrderTraversal(root.left, result);
+        preOrderTraversal(root.right, result);
+    }
     
 }
