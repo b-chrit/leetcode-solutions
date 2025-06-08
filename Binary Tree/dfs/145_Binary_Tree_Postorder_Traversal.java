dc19449581e43b68068ed1cf095bb5358a2c9b5a
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-07 21:02:02 -0400

    Add solution for 145

--- Binary Tree/dfs/145_Binary_Tree_Postorder_Traversal.java
+++ Binary Tree/dfs/145_Binary_Tree_Postorder_Traversal.java
@@ -1,3 +1,33 @@
-public class 145_Binary_Tree_Postorder_Traversal {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        postOrderTraversal(root, result);
+        return result;
+    }
+
+    private void postOrderTraversal(TreeNode root, List<Integer> result){
+        if(root == null) return;
+
+        postOrderTraversal(root.left, result);
+        postOrderTraversal(root.right, result);
+        result.add(root.val);
+    }
+
 }
