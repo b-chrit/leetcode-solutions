acdb52b164cb1c2d8dfe372d409ad420088e567a
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-08 08:33:35 -0400

    Add solution for 232

--- Arrays and Hashing/232_Implement_Queue_using_Stacks.java
+++ Arrays and Hashing/232_Implement_Queue_using_Stacks.java
@@ -1,3 +1,53 @@
-public class 232_Implement_Queue_using_Stacks {
+class MyQueue {
+    Stack<Integer> first = new Stack<>();
+    Stack<Integer> second = new Stack<>();
+
+    public MyQueue() {
+        
+    }
     
+    public void push(int x) {
+        first.push(x);
+    }
+    
+    public int pop() {
+        if(empty()){
+            return -1;
+        }
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        int removed = second.pop();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return removed;
+    }
+    
+    public int peek() {
+         if(empty()){
+            return -1;
+        }
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        int peekItem = second.peek();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return peekItem;
+    }
+    
+    public boolean empty() {
+        return first.isEmpty();
+    }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
