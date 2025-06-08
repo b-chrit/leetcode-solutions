aaed4c97eb4564054a1f5ae46ae454689124db35
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-07 21:00:55 -0400

    Add solution for 94 - recursive

--- Binary Tree/dfs/94_Binary_Tree_Inorder_Traversal.java
+++ Binary Tree/dfs/94_Binary_Tree_Inorder_Traversal.java
@@ -21,4 +21,23 @@ class Solution {
 
         return result;
     }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        inorderTraversal(root, result);
+
+        return result;
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> result){
+        if(root == null) return;
+
+        inorderTraversal(root.left, result);
+
+        result.add(root.val);
+
+        inorderTraversal(root.right, result);
+    }
 }
