43c719df434a8ae7db95e203a9289aab07ee62a8
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-07 21:00:28 -0400

    Add solution for 94

--- Binary Tree/dfs/94_Binary_Tree_Inorder_Traversal.java
+++ Binary Tree/dfs/94_Binary_Tree_Inorder_Traversal.java
@@ -1,3 +1,24 @@
-public class 94_Binary_Tree_Inorder_Traversal {
-    
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Stack<TreeNode> myStack = new Stack<>();
+        TreeNode current = root;
+
+        while(current != null || !myStack.isEmpty()){
+
+            while(current != null){
+                myStack.push(current);
+                current = current.left;
+            }
+
+            current = myStack.pop();
+            result.add(current.val);
+
+            current = current.right;
+        }
+
+        return result;
+    }
 }
