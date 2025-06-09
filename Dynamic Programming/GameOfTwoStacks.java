0f00e6829308fabd1adf19d22f0fb16981ca8404
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-09 08:27:20 -0400

    Add solution for gameOfTwoStacks - HackerRank

--- Dynamic Programming/GameOfTwoStacks.java
+++ Dynamic Programming/GameOfTwoStacks.java
@@ -7,4 +7,38 @@
     Space Complexity: O(n × m × maxSum)
  */
 public class GameOfTwoStacks {
+    
+    public static int maxElementsFromTwoStacks(int maxSum, int[] stackA, int[] stackB) {
+        Map<String, Integer> cache = new HashMap<>();
+        return findMaxElements(stackA, stackB, 0, 0, maxSum, cache);
+    }
+    
+    private static int findMaxElements(int[] stackA, int[] stackB, int indexA, int indexB, 
+                                     int remainingSum, Map<String, Integer> cache) {
+        if (indexA >= stackA.length && indexB >= stackB.length) {
+            return 0;
+        }
+        
+        String currentState = indexA + "," + indexB + "," + remainingSum;
+        if (cache.containsKey(currentState)) {
+            return cache.get(currentState);
+        }
+        
+        int maxElementsCount = 0;
+        
+        if (indexA < stackA.length && stackA[indexA] <= remainingSum) {
+            maxElementsCount = Math.max(maxElementsCount, 
+                1 + findMaxElements(stackA, stackB, indexA + 1, indexB, 
+                                  remainingSum - stackA[indexA], cache));
+        }
+        
+        if (indexB < stackB.length && stackB[indexB] <= remainingSum) {
+            maxElementsCount = Math.max(maxElementsCount, 
+                1 + findMaxElements(stackA, stackB, indexA, indexB + 1, 
+                                  remainingSum - stackB[indexB], cache));
+        }
+        
+        cache.put(currentState, maxElementsCount);
+        return maxElementsCount;
+    }
 }
