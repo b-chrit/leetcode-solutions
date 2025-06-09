de28c482a6b7e2f68ff6e37787cb3ab5fc34b309
Baraa Chrit <baraaadnan01@gmail.com>
2025-06-09 08:26:45 -0400

    Add solution for gameOfTwoStacks - HackerRank

---
+++ Dynamic Programming/GameOfTwoStacks.java
@@ -0,0 +1,10 @@
+/*
+ *  Problem: Find the maximum number of elements you can remove from two stacks such that their sum doesn't exceed a given maximum.
+    (https://www.hackerrank.com/challenges/game-of-two-stacks/problem)
+    Approach: Recursive solution with memoization that explores taking elements from either stack at each step.
+
+    Time Complexity: O(n × m × maxSum)
+    Space Complexity: O(n × m × maxSum)
+ */
+public class GameOfTwoStacks {
+}
