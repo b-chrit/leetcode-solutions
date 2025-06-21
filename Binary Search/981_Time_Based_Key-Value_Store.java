7e979b7a804c782fd1fe6d88b3986bf4114fe63b
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-06-21 10:33:29 -0400

    Add solution for 981

--- Binary Search/981_Time_Based_Key-Value_Store.java
+++ Binary Search/981_Time_Based_Key-Value_Store.java
@@ -7,3 +7,26 @@ class Pair {
         this.value = value;
     }
 }
+
+class TimeMap {
+    
+    private Map<String, List<Pair>> map;
+    
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair(timestamp, value));
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
