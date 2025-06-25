fb2a896613ec24ccb71d2c27e0e06a5b488b9939
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-06-25 06:34:53 -0400

    Add solution for 211

--- Arrays and Hashing/211_Design_Add_and_Search_Words_Data_Structure.java
+++ Arrays and Hashing/211_Design_Add_and_Search_Words_Data_Structure.java
@@ -1,1 +1,40 @@
 
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+        
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+    
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        TrieNode current = root;
+        
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+            current = current.children[index];
+        }
+        
+        current.isEndOfWord = true;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
