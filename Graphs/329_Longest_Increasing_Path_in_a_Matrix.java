6df103318a83352a9904e789084e7b5b9aa4ed96
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-14 10:10:38 -0400

    Add solution for 329

--- Graphs/329_Longest_Increasing_Path_in_a_Matrix.java
+++ Graphs/329_Longest_Increasing_Path_in_a_Matrix.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    private Map<String, Integer> memo;
+    private int[][] matrix;
+    private int m, n;
+    private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        
+        this.matrix = matrix;
+        this.m = matrix.length;
+        this.n = matrix[0].length;
+        this.memo = new HashMap<>();
+        
+        int maxPath = 1;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxPath = Math.max(maxPath, dfs(i, j));
+            }
+        }
+        
+        return maxPath;
+    }
+}
