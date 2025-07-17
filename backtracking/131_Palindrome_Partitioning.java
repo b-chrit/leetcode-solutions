d36cab9b0f004b5028e03cc5e3b12ac0df2c8a35
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-17 11:59:44 -0400

    Add solution for 131

--- backtracking/131_Palindrome_Partitioning.java
+++ backtracking/131_Palindrome_Partitioning.java
@@ -1,1 +1,25 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        backtrack(s, 0, current, result);
+        return result;
+    }
+    
+    private void backtrack(String s, int start, List<String> current, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        for (int end = start; end < s.length(); end++) {
+            String substring = s.substring(start, end + 1);
 
+            if (isPalindrome(substring)) {
+                current.add(substring);
+                backtrack(s, end + 1, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+}
