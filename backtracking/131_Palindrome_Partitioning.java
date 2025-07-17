ebb043af1ff2ad4e87ea759b9aaec29f5d55395c
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-17 11:59:55 -0400

    Add solution for 131

--- backtracking/131_Palindrome_Partitioning.java
+++ backtracking/131_Palindrome_Partitioning.java
@@ -22,4 +22,18 @@ class Solution {
             }
         }
     }
+    
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
