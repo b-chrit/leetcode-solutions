ffcc827ca8a713323c2abd0c8d070a94ed3fd0a2
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-19 11:27:54 -0400

    Add solution for 332

--- Graphs/332_Reconstruct_Itinerary.java
+++ Graphs/332_Reconstruct_Itinerary.java
@@ -1,1 +1,32 @@
-
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        
+        for (List<String> ticket : tickets) {
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            
+            if (!graph.containsKey(from)) {
+                graph.put(from, new PriorityQueue<>());
+            }
+            graph.get(from).offer(to);
+        }
+        
+        List<String> result = new ArrayList<>();
+        dfs("JFK", graph, result);
+        
+        Collections.reverse(result);
+        return result;
+    }
+    
+    private void dfs(String airport, Map<String, PriorityQueue<String>> graph, List<String> result) {
+        PriorityQueue<String> destinations = graph.get(airport);
+        
+        while (destinations != null && !destinations.isEmpty()) {
+            String nextAirport = destinations.poll();
+            dfs(nextAirport, graph, result);
+        }
+        
+        result.add(airport);
+    }
+}
