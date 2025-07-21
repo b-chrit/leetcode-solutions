24d1902551b504f57ef6c07ad831641c3caea80f
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-21 00:44:40 -0400

    Add solution for 778

--- Heaps/778_Swim_in_Rising_Water.java
+++ Heaps/778_Swim_in_Rising_Water.java
@@ -1,1 +1,11 @@
+class Solution {
+    class Cell {
+        int row, col, elevation;
 
+        Cell(int row, int col, int elevation) {
+            this.row = row;
+            this.col = col;
+            this.elevation = elevation;
+        }
+    }
+}
