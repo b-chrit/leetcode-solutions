91357874f63841d7435cdf0c77c03f375d12f16a
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-23 17:55:04 -0400

    Add solution for 42 - subOptimal

--- Two Pointers/42_Trapping_Rain_Water.java
+++ Two Pointers/42_Trapping_Rain_Water.java
@@ -25,3 +25,30 @@ class Solution {
         return total;
     }
 }
+
+class SolutionSuboptimal {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        
+        int n = height.length;
+        int[] maxLeft = new int[n];
+        int[] maxRight = new int[n];
+        
+        maxLeft[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i-1], height[i]);
+        }
+        
+        maxRight[n-1] = height[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i+1], height[i]);
+        }
+        
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += Math.min(maxLeft[i], maxRight[i]) - height[i];
+        }
+        
+        return total;
+    }
+}
