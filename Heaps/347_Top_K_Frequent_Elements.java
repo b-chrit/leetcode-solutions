78e8f0ba0656a6c8fc825c23f2c0fdc3ea3c9c2b
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-26 11:45:06 -0400

    Add solution for 347 - maxHeap

--- Heaps/347_Top_K_Frequent_Elements.java
+++ Heaps/347_Top_K_Frequent_Elements.java
@@ -1,1 +1,23 @@
-
+// Time: O(n log n) - adding n elements to heap
+// Space: O(n) - heap stores all unique elements
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(
+            (a, b) -> freqMap.get(b) - freqMap.get(a)
+        );
+        
+        maxHeap.addAll(freqMap.keySet());
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+        
+        return result;
+    }
+}
