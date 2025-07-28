eebbf2412fa329e849a351ad0baf4d28af521b19
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-07-28 19:34:41 -0400

    Add solution for 234

--- Linked List/234_Palindrome_Linked_List.java
+++ Linked List/234_Palindrome_Linked_List.java
@@ -1,1 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode mid = getMid(head);
+
+        ListNode secondHalf = reverse(mid);
+
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode getMid(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode prev = null;
+
+        while (node != null) {
+            ListNode temp = node.next;
+            node.next = prev;
+            prev = node;
+            node = temp;
+        }
+
+        return prev;
+    }
+}
 
