076d40571b1c07412cfe316a1465aff7ca8a7605
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-08-02 20:56:24 -0400

    Add solution for 9 - reversal

--- Arrays and Hashing/9_Palindrome_Number.java
+++ Arrays and Hashing/9_Palindrome_Number.java
@@ -15,14 +15,19 @@ class SolutionStringTwoPointer {
 
 class SolutionHalfReversal {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-        int reversed = 0;
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
 
-        return x == reversed || x == reversed / 10;
+        int reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            int lastDigit = x % 10;
+            reversed = reversed * 10 + lastDigit;
+            x = x / 10;
+        }
+
+        return original == reversed;
     }
 }
