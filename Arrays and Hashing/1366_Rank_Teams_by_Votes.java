b513bd011917bc9f537db60939f5ac1e0d1513ee
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-08-12 19:30:02 -0400

    Add solution for 1366

--- Arrays and Hashing/1366_Rank_Teams_by_Votes.java
+++ Arrays and Hashing/1366_Rank_Teams_by_Votes.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    class Team {
+        char name;
+        int[] votes;
+        
+        Team(char name, int positions) {
+            this.name = name;
+            this.votes = new int[positions];
+        }
+    }
+}
