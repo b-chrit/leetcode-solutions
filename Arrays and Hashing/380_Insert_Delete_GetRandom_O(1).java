3979847cbaa913165f36852c3be8e68d2cf929af
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-08-13 17:54:38 -0400

    Add solution for 380

--- Arrays and Hashing/380_Insert_Delete_GetRandom_O(1).java
+++ Arrays and Hashing/380_Insert_Delete_GetRandom_O(1).java
@@ -1,1 +1,40 @@
-
+public class RandomizedSet {
+    private List<Integer> values;
+    private Map<Integer, Integer> indices;
+    private Random random;
+    
+    public RandomizedSet() {
+        values = new ArrayList<>();
+        indices = new HashMap<>();
+        random = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if (indices.containsKey(val)) {
+            return false;
+        }
+        values.add(val);
+        indices.put(val, values.size() - 1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!indices.containsKey(val)) {
+            return false;
+        }
+        int indexToRemove = indices.get(val);
+        int lastElement = values.get(values.size() - 1);
+        
+        values.set(indexToRemove, lastElement);
+        indices.put(lastElement, indexToRemove);
+        
+        values.remove(values.size() - 1);
+        indices.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        int randomIndex = random.nextInt(values.size());
+        return values.get(randomIndex);
+    }
+}
