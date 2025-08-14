16089c72144559f3909e9d9924ed2e37c9d8c7bd
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-08-14 15:07:57 -0400

    Add UnitConverter solution - bfs

--- Graphs/Unit_Converter.java
+++ Graphs/Unit_Converter.java
@@ -52,3 +52,70 @@ class UnitConverterDFS {
         System.out.println(converter.convert(1, "m", "ft"));
     }
 }
+
+class UnitConverterBFS {
+    class Edge {
+        String to;
+        double factor;
+        
+        Edge(String to, double factor) {
+            this.to = to;
+            this.factor = factor;
+        }
+    }
+    
+    class State {
+        String unit;
+        double multiplier;
+        
+        State(String unit, double multiplier) {
+            this.unit = unit;
+            this.multiplier = multiplier;
+        }
+    }
+    
+    private final Map<String, List<Edge>> graph = new HashMap<>();
+    
+    public void addConversion(String from, String to, double factor) {
+        if (!graph.containsKey(from)) graph.put(from, new ArrayList<>());
+        if (!graph.containsKey(to)) graph.put(to, new ArrayList<>());
+        graph.get(from).add(new Edge(to, factor));
+        graph.get(to).add(new Edge(from, 1.0 / factor));
+    }
+    
+    public double convert(String from, String to, double amount) {
+        if (!graph.containsKey(from) || !graph.containsKey(to)) return -1.0;
+        if (from.equals(to)) return amount;
+        
+        Queue<State> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        
+        queue.offer(new State(from, 1.0));
+        visited.add(from);
+        
+        while (!queue.isEmpty()) {
+            State current = queue.poll();
+            
+            for (Edge edge : graph.get(current.unit)) {
+                double newMultiplier = current.multiplier * edge.factor;
+                
+                if (edge.to.equals(to)) {
+                    return amount * newMultiplier;
+                }
+                
+                if (!visited.contains(edge.to)) {
+                    visited.add(edge.to);
+                    queue.offer(new State(edge.to, newMultiplier));
+                }
+            }
+        }
+    
+    public static void main(String[] args) {
+        UnitConverterBFS converter = new UnitConverterBFS();
+        converter.addConversion("m", "ft", 3.28084);
+        converter.addConversion("ft", "in", 12);
+        
+        System.out.println(converter.convert(1, "m", "in"));
+        System.out.println(converter.convert(1, "m", "ft"));
+    }
+}
