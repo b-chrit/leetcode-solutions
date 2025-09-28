05dcc1c3f075877e36dbc1fa948cf1f72e8a6038
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-09-28 16:35:49 -0400

    Add solution for 1152

--- Arrays and Hashing/1152_Analyze_User_Website_Visit_Pattern.java
+++ Arrays and Hashing/1152_Analyze_User_Website_Visit_Pattern.java
@@ -11,5 +11,21 @@ public class Solution {
             site = s;
         }
     }
-  
+
+    public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
+        List<Visit> visits = new ArrayList<>();
+        for (int i = 0; i < username.length; i++) {
+            visits.add(new Visit(username[i], timestamp[i], website[i]));
+        }
+
+        visits.sort((a, b) -> a.time - b.time);
+
+        Map<String, List<String>> userToSites = new HashMap<>();
+        for (Visit v : visits) {
+            if (!userToSites.containsKey(v.user)) {
+                userToSites.put(v.user, new ArrayList<>());
+            }
+            userToSites.get(v.user).add(v.site);
+        }
+    }
 }
