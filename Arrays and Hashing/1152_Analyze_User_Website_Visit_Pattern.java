e3d1727f49e7015efcbb9ff1f74a8cc5ce7fa550
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-09-28 16:35:02 -0400

    Add solution for 1152

---
+++ Arrays and Hashing/1152_Analyze_User_Website_Visit_Pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    class Visit {
+        String user;
+        int time;
+        String site;
+        Visit(String u, int t, String s) {
+            user = u;
+            time = t;
+            site = s;
+        }
+    }
+  
+}
