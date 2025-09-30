e112b29a1ccb9e96747e7f5d0a78f3838970965f
Baraa Chrit <124475406+b-chrit@users.noreply.github.com>
2025-09-30 15:39:29 -0400

    Add solution for 404

---
+++ Binary Tree/bfs/404_Sum_of_Left_Leaves.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                if (node.left.left == null && node.left.right == null) {
+                    sum += node.left.val;
+                } else {
+                    queue.add(node.left);
+                }
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return sum;
+    }
+}
